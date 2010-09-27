9428c59af2a0558f6f92aeb80f27183bb6ea9727
Andreas Fischer <bantu@phpbb.com>
2010-09-27 10:44:55 +0200

    [ticket/9425]
    PHPBB3-9425

--- tests/regex/url.php
+++ tests/regex/url.php
@@ -12,23 +12,58 @@ require_once '../phpBB/includes/functions.php';
 
 class phpbb_regex_url_test extends phpbb_test_case
 {
-	public function url_test_data()
+	protected $regex;
+
+	public function setUp()
+	{
+		$this->regex = '#^' . get_preg_expression('url') . '$#i';
+	}
+
+	public function positive_match_data()
 	{
 		return array(
-			array('http://www.phpbb.com/community/', 1),
-			array('http://www.phpbb.com/path/file.ext#section', 1),
-			array('ftp://ftp.phpbb.com/', 1),
-			array('sip://bantu@phpbb.com', 1),
+			array('http://www.phpbb.com:81/community/'),
+			array('http://www.phpbb.com/path/file.ext#section'),
+			array('ftp://ftp.phpbb.com/'),
+			array('sip://bantu@phpbb.com'),
 
-			array('www.phpbb.com/community/', 0),
+			// IPv4
+			array('http://192.168.1.1:81/community/'),
+			array('http://192.168.1.1/path/file.ext#section'),
+			array('ftp://192.168.1.1/'),
+			array('sip://bantu@192.168.1.1'),
+
+			// IPv6 - from rfc2732
+			array('http://[FEDC:BA98:7654:3210:FEDC:BA98:7654:3210]:80/index.html'),
+			array('http://[1080:0:0:0:8:800:200C:417A]/index.html'),
+			array('http://[3ffe:2a00:100:7031::1]'),
+			array('http://[1080::8:800:200C:417A]/foo'),
+			array('http://[::192.9.5.5]/ipng'),
+			array('http://[::FFFF:129.144.52.38]:80/index.html'),
+			array('http://[2010:836B:4179::836B:4179]'),
+		);
+	}
+
+	public function negative_match_data()
+	{
+		return array(
+			array('www.phpbb.com/community/'),
 		);
 	}
 
 	/**
-	* @dataProvider url_test_data
+	* @dataProvider positive_match_data
 	*/
-	public function test_url($url, $expected)
+	public function test_positive_match($string)
 	{
-		$this->assertEquals($expected, preg_match('#^' . get_preg_expression('url') . '$#i', $url));
+		$this->assertEquals(1, preg_match($this->regex, $string));
+	}
+
+	/**
+	* @dataProvider negative_match_data
+	*/
+	public function test_negative_match($string)
+	{
+		$this->assertEquals(0, preg_match($this->regex, $string));
 	}
 }
